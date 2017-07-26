b03b3c44680c61a1a67470917b512f08fb1046b3
Imran <imrankst1221@gmail.com>
2017-07-26 12:29:22 +0600

    update.

--- Websiteinwebview/app/src/main/java/imrankst1221/website/in/webview/MainActivity.java
+++ Websiteinwebview/app/src/main/java/imrankst1221/website/in/webview/MainActivity.java
@@ -1,8 +1,5 @@
 package imrankst1221.website.in.webview;
 
-import android.graphics.Color;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -50,11 +47,6 @@ public class MainActivity extends Activity {
         prgs = (ProgressBar) findViewById(R.id.progressBar);
         main_layout = (RelativeLayout) findViewById(R.id.main_layout);
 
-        GradientDrawable rainbow = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[] {Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED});
-        AnimationDrawable ad = getProgressBarAnimation();
-        prgs.setBackgroundDrawable(ad);
-
         // splash screen View
         if (!showProgressOnSplashScreen)
             ((ProgressBar) findViewById(R.id.progressBarSplash)).setVisibility(View.GONE);
@@ -66,7 +58,7 @@ public class MainActivity extends Activity {
                 if (splash.getVisibility() == View.VISIBLE)
                     splash.setVisibility(View.GONE);
             }
-        },2000);
+        },5000);
 
 //		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 //
@@ -138,37 +130,6 @@ public class MainActivity extends Activity {
 
         });
 
-    }
-
-    private AnimationDrawable getProgressBarAnimation(){
-        GradientDrawable rainbow1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[] {Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW});
-
-        GradientDrawable rainbow2 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[] { Color.YELLOW, Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN});
-
-        GradientDrawable rainbow3 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[] { Color.GREEN, Color.YELLOW, Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN });
-
-        GradientDrawable rainbow4 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[] { Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED, Color.MAGENTA, Color.BLUE });
-
-        GradientDrawable rainbow5 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[] { Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED, Color.MAGENTA });
-
-        GradientDrawable rainbow6 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[] {Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED });
-
-
-        GradientDrawable[]  gds = new GradientDrawable[] {rainbow1, rainbow2, rainbow3, rainbow4, rainbow5, rainbow6};
-
-        AnimationDrawable animation = new AnimationDrawable();
-
-        for (GradientDrawable gd : gds){
-            animation.addFrame(gd, 100);
-        }
-        animation.setOneShot(false);
-        return animation;
     }
 
     /**
